9cc33d7ab16d9dfceb24bde85d04106e79c6ccad
Karácsonyi Deniel <h765660@stud.u-szeged.hu>
2021-03-13 10:25:41 +0100

    deck change works

--- index.js
+++ index.js
@@ -11,86 +11,86 @@ let basicCard;
 
 
 cardsArray = [
-          {
-        src: "./images/cards/pl.png", //background
-        id: 0
-    },    {
-        src: "./images/cards/pl.png",
+        {
+        //src: "./images/pl.png",
         srcBack: "./images/cards/arsenal.png",
         id: 1
     },    {
-        src: "./images/cards/astonvilla.png",
+        srcBack: "./images/cards/astonvilla.png",
         id: 2
     },    {
-        src: "./images/cards/bha.png",
+        srcBack: "./images/cards/bha.png",
         id: 3
     },    {
-        src: "./images/cards/burnley.png",
+        srcBack: "./images/cards/burnley.png",
         id: 4
     },    {
-        src: "./images/cards/chelsea.png",
+        srcBack: "./images/cards/chelsea.png",
         id: 5
     },    {
-        src: "./images/cards/cpalace.png",
+        srcBack: "./images/cards/cpalace.png",
         id: 6
     },    {
-        src: "./images/cards/everton.png",
+        srcBack: "./images/cards/everton.png",
         id: 7
     },    {
-        src: "./images/cards/fulham.png",
+        srcBack: "./images/cards/fulham.png",
         id: 8
     },    {
-        src: "./images/cards/leeds.png",
+        srcBack: "./images/cards/leeds.png",
         id: 9
     },    {
-        src: "./images/cards/leicester.png",
+        srcBack: "./images/cards/leicester.png",
         id: 10
     },    {
-        src: "./images/cards/liverpool.png",
+        srcBack: "./images/cards/liverpool.png",
         id: 11
     },    {
-        src: "./images/cards/manc.png",
+        srcBack: "./images/cards/manc.png",
         id: 12
     },    {
-        src: "./images/cards/manu.png",
+        srcBack: "./images/cards/manu.png",
         id: 13
     },    {
-        src: "./images/cards/newcastle.png",
+        srcBack: "./images/cards/newcastle.png",
         id: 14
     },    {
-        src: "./images/cards/sheffield.png",
+        srcBack: "./images/cards/sheffield.png",
         id: 15
     },    {
-        src: "./images/cards/southhampton.png",
+        srcBack: "./images/cards/southhampton.png",
         id: 16
     },    {
-        src: "./images/cards/tottenham.png",
+        srcBack: "./images/cards/tottenham.png",
         id: 17
     },    {
-        src: "./images/cards/wba.png",
+        srcBack: "./images/cards/wba.png",
         id: 18
     },    {
-        src: "./images/cards/westham.png",
+        srcBack: "./images/cards/westham.png",
         id: 19
     },    {
-        src: "./images/cards/wolverhampton.png",
+        srcBack: "./images/cards/wolverhampton.png",
         id: 20
     },
 ];
 
 
 let newArray = [];
-const sourceCard = "./images/cards/pl.png";
+const sourceCard = "./images/pl.png";
 
 $(document).ready(function(){//jquery start
-
+console.log(newArray)
 gameArea = $("#gameArea");
 gameAreaWidth = gameArea.width();
 gameAreaHeight = gameArea.height();
 
+
+
+//test
 let test_img = $("<img />",{
     id: "imgId"+cardsArray[1].id,
-    src: cardsArray[1].src,
+    src: sourceCard,
     srcBack: cardsArray[1].srcBack
 })
 gameArea.append(test_img);
@@ -104,56 +104,49 @@ gameArea.append(test_img);
             $(this).attr("src", sourceCard );
         }
     });
+//VEGE TEST
+
+    $("#newGame").on("click", function(){
+        if(newArray.length != 0){
+            newArray = [];
+            console.log("lefutott az if")
+            $("#gameArea").empty();
+            newArray = createMap()
+        }else{
+            console.log("lefutott az else")
+             newArray = createMap()
+        }
+        console.log(newArray.length)
+
+        for(let i=0; i<newArray.length; i++) {
+
+            let test_img = $("<img />",{
+                id: "imgId"+i,
+                trueImageId: newArray[i].id,
+                src: sourceCard,
+                srcBack: newArray[i].srcBack
+            })
+            gameArea.append(test_img);
+
+            $("#imgId"+i).on("click", function (){
+                console.log($(this).attr("id"))
+                if($(this).attr("src") === sourceCard){
+                    $(this).attr("src", $(this).attr("srcBack") );
+                }
+                else{
+                    $(this).attr("src", sourceCard );
+                }
+            });
 
 
-//new game
-//     $("#newGame").on("click", function(){
-//         if(newArray.length != 0){
-//             newArray = [];
-//             console.log("lefutott az if")
-//             $("#gameArea").empty();
-//             newArray = createMap()
-//         }else{
-//             console.log("lefutott az else")
-//              newArray = createMap()
-//         };
-//         console.log("Megnyomtak")
-//        // newArray = createMap();
-//
-//         for(let i=0; i<newArray.length; i++) {
-//             let currentCard = $('<img />', {
-//                 //     currentId: i,
-//                 src: newArray[i].src,
-//                 //     border: 1,
-//                 //     margin: 20,
-//                 //     padding: 20,
-//                      width: 100,
-//                     height: 100
-//             });
-//             gameArea.append(currentCard);
-//
-//             currentCard.on("click", function(){
-//                 console.log("megnyomtak", $(this).attr("currentId"))
-//             });
-//         }
-//     })
+        }
+    })
 
 
 
 // pálya legenerálás
-    function createMap(){
-         newArray = [];
-        const cardsCopy = [...cardsArray];
-         for(let i=0; i<6; i++){
-             let randomNumber = Math.floor(Math.random()*cardsCopy.length);
-             newArray.push(cardsCopy[randomNumber]);
-             cardsCopy.splice(randomNumber, 1);
-             newArray[i].id=i;
-         }
-        console.log(newArray)
 
-        return newArray;
-    }
+
 
 
 
@@ -161,9 +154,41 @@ gameArea.append(test_img);
 
 }); //
 
+// a memória játék 6 elemének kisorsolása 20 kártya közül
+function createMap(){
+    newArray = [];
+    const cardsCopy = [...cardsArray]; // eredeti kártya tömb másolása
+    for(let i=0; i<6; i++){
+        let randomNumber = Math.floor(Math.random()*cardsCopy.length);
+        newArray.push(cardsCopy[randomNumber]);
+        newArray.push(cardsCopy[randomNumber]);
+
+        cardsCopy.splice(randomNumber, 1); // a kiválasztott elem törlése a másolt tömbből hogy ne ismétlődhessen
+        newArray[i].id=i;
 
 
 
+    }
+    newArray.sort( (a,b) => .5 - Math.random() );;
+   // randomizeOrder(newArray);
+    return  newArray;
+
+}
+//a createMapbol kirandomizált tömb sorrendjének összekeverése
+// function randomizeOrder(array){
+//
+//     // for(let i=array.length-5; i>0; i--){
+//     //     const j = Math.floor(Math.random() * (i));
+//     //     const tmp = array[i];
+//     //     array[i] = array[j];
+//     //     array[j] = tmp;
+//     // }
+//     for(let i=0; i<array.length; i+=2){
+//         array.sort( (a,b) => .5 - Math.random());
+//     }
+// }
+
+console.log(createMap());
 
 
 
